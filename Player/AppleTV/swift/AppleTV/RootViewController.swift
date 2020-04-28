3a703eeaaba369a9ac6da90e9bdaf0ca71fc6134
Bart Jacobs <git@codefoundry.be>
2020-04-28 12:27:05 +0200

    Create, implement, and integrate RootViewController

---
+++ Player/AppleTV/swift/AppleTV/RootViewController.swift
@@ -0,0 +1,11 @@
+//
+//  RootViewController.swift
+//  AppleTV
+//
+//  Created by Bart Jacobs on 28/04/2020.
+//  Copyright © 2020 Brightcove. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {}
